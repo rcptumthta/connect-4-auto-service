8b74cc4a6717bc95d95e50e7ccdab9f4b519f64d
rcptumthta <rcptum.thta@outlook.com>
2024-10-17 16:32:44 +0700

    build: set up configurations

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const path = require("node:path");
+const sourceMapSupport = require("source-map-support");
+
+sourceMapSupport.install({ environment: "node" });
+
+module.exports = function compile(option) {
+  return {
+    ...option,
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      libraryTarget: "commonjs2"
+    },
+    target: "node",
+    devtool: "source-map",
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+    optimization: {
+      ...option.optimization,
+      minimizer: [
+        new TerserWebpackPlugin({
+          extractComments: false,
+          parallel: true,
+          terserOptions: {
+            ecma: 2020,
+            keep_classnames: true,
+            keep_fnames: true
+          }
+        })
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "resources",
+            to: "resources"
+          }
+        ]
+      })
+    ]
+  };
+}
